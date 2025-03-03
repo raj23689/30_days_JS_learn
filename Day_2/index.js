a08405aff975e0314a11d99e1ba73c10272763e1
raj23689 <92665411+raj23689@users.noreply.github.com>
2025-03-03 23:10:29 +0530

    Adding loading the numbers to select on HTML.

--- Day_2/index.js
+++ Day_2/index.js
@@ -1,1 +1,13 @@
-console.log("Loaded")
+function loadNumbers(selectElement) {
+    for (let index = 0; index <=100; index++) {
+        const option = document.createElement('option');
+        option.value = index;
+        option.textContent = index;
+        selectElement.appendChild(option);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadNumbers(document.getElementById('select1'));
+    loadNumbers(document.getElementById('select2'));
+});
